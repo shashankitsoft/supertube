b7ee4d9262a988fdedd844f4e5459b8125997556
Shashank <shashank.itsoft@gmail.com>
2025-05-10 19:01:50 +0530

    Updates

--- assets/md.transition-legacy-B4SZADEI.js
+++ assets/md.transition-legacy-B4SZADEI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DGFMAVTi.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-X_zWKxzr.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Bb8_xsat.js
+++ assets/swipe-back-legacy-Bb8_xsat.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DGFMAVTi.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-X_zWKxzr.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
