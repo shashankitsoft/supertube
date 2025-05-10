925f026c6deb4e0bf2e5c465c15ecc1092c333c6
Shashank <shashank.itsoft@gmail.com>
2025-05-10 20:45:14 +0530

    Updates

--- assets/ios.transition-legacy-BaQRXANm.js
+++ assets/ios.transition-legacy-BaQRXANm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-X_zWKxzr.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DA8hOtJU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-DKAm9cvg.js
+++ assets/status-tap-legacy-DKAm9cvg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-X_zWKxzr.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-DA8hOtJU.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
