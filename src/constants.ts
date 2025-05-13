bb68f75a846e1995a4be3faa058535754050ee54
Shashank <shashank.itsoft@gmail.com>
2025-05-13 12:43:18 +0530

    fix env variable

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
+// console.log("env meta",import.meta.env);
 export const BASE_PATH = import.meta.env.VITE_BASE_PATH || '/';
-export const REMOTE_BASE_URL = import.meta.env.REMOTE_BASE_URL || "https://shashankitsoft.github.io/supertube/";
+export const REMOTE_BASE_URL = import.meta.env.VITE_REMOTE_BASE_URL || "https://shashankitsoft.github.io/supertube/";
+
