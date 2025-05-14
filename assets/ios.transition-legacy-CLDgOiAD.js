5510af3e66f3c2bb98544b5e442efd7d55e91aea
shashankitsoft <shashankitsoft@users.noreply.github.com>
2025-05-14 11:06:23 +0000

    deploy: eb443daf651f52087f56d1c6163d563ae9211b7d

--- assets/ios.transition-legacy-CLDgOiAD.js
+++ assets/ios.transition-legacy-CLDgOiAD.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BMMFPsfZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-pk28gyWH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
