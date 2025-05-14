9d9328419189b4f9616f0f6ba930778b354546de
shashankitsoft <shashankitsoft@users.noreply.github.com>
2025-05-14 06:53:53 +0000

    deploy: 9d8dd4f072b66272ee7ed0fc6d3aaf6d68e0f3a4

--- assets/md.transition-legacy-DDMT6dlG.js
+++ assets/md.transition-legacy-DDMT6dlG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cqz_h22H.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-vwhBdjqH.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
