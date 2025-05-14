343394b72f390351f712cfdf7c928f3843d748b0
shashankitsoft <shashankitsoft@users.noreply.github.com>
2025-05-14 07:52:05 +0000

    deploy: 0f2f5505423f944f85374a8094d9ec92cc27af51

--- assets/swipe-back-legacy-R5K7Wmgz.js
+++ assets/swipe-back-legacy-R5K7Wmgz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-vwhBdjqH.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BMMFPsfZ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
